8dc0de0827cd35acac02ed4458a0eae2ed80c254
Felix Tubiana <felixtubiana@gmail.com>
2018-02-09 03:11:31 +0000

    Version 0.07

--- lib/Devel/Scope.pm
+++ lib/Devel/Scope.pm
@@ -13,7 +13,7 @@ require Exporter;
 our @ISA       = qw( Exporter );
 our @EXPORT_OK = qw( debug debug_enable debug_disable );
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 my %default_config = (
     'DEVEL_SCOPE_DEPTH'                => 0,
@@ -147,7 +147,7 @@ Devel::Scope - Scope based debug
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =head1 SYNOPSIS
 
